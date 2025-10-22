a8e6c46f5c6ea44b9655a4856948cdee5fe4f671
rodeofly <florian.tobe@gmail.com>
2025-10-22 17:28:32 +0400

    gros bug

--- backups/mathalea_1761116157116/lib/stores/storage.ts
+++
@@ -1,9 +0,0 @@
-export function isLocalStorageAvailable() {
-  try {
-    window.localStorage.setItem('__test__', '__test__')
-    window.localStorage.removeItem('__test__')
-    return true
-  } catch (e) {
-    return false
-  }
-}
--- backups/mathalea_1761117524966/modules/declarations/jszip-utils.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'jszip-utils' {
-  export function getBinaryContent(
-    url: string,
-    callback: (error: Error | null, data: ArrayBuffer | null) => void,
-  ): void
-}
