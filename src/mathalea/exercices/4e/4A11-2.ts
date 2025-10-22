89c3dc961bbb3311a5fd5b5819f11276d4686088
rodeofly <florian.tobe@gmail.com>
2025-10-22 17:25:43 +0400

    gros bug

--- src/mathalea/exercices/4e/4A11-2.ts
+++
@@ -1,9 +0,0 @@
-import FractionsIrreductibles from '../3e/3A11'
-export const titre = 'Fractions irréductibles'
-export const interactifReady = false
-export const uuid = '426f5'
-export const refs = {
-  'fr-fr': ['4A11-2'],
-  'fr-ch': [],
-}
-export default class FractionsIrreductibles4e extends FractionsIrreductibles {}
--- src/mathalea/lib/components/images.ts
+++
@@ -1,6 +0,0 @@
-export const doesImageExist = (path: string): boolean => {
-  const http = new XMLHttpRequest()
-  http.open('HEAD', path, false)
-  http.send()
-  return http.status !== 404
-}
